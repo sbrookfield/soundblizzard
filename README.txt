153ca02859d4f133dc471f222c3c651c6946d03f
Sam Brookfield <sbrookfield@gmail.com>
2012-04-08 18:46:17 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+SoundBlizzard Media Player
+
+Open Source Python Media Player
+http://code.google.com/p/soundblizzard/
+
+
---
+++ src/sb-config-gsettings.py
@@ -0,0 +1,30 @@
+'''
+Created on 8 Apr 2012
+
+@author: sam
+'''
+from gi.repository import Gio
+#Bind Gsettings
+class sb_config_gsettings(object):
+  '''Module which hold configuration settings using gconf backend
+    Requires /usr/share/glib-2.0/schemas/apps.sb-config.gschema.xml to be in place
+    and run glib-compile-schemas /usr/share/glib-2.0/schemas/ once file present
+    Module is type specific as appears to be the convention
+    Thanks to http://www.micahcarrick.com/gsettings-python-gnome-3.html'''
+
+  def __init__(self):
+    '''Initialises class'''
+    from gi.repository import Gio
+    self.settings = Gio.Settings.new("apps.soundblizzard")    
+  def test(self):
+    dbfile = conf.settings.get_string('dbfile')
+    dbfile = Gio.File.new(path=dbfile)
+    if (dbfile.query_file_type != gio.FILE_TYPE_REGULAR):
+        print('dbfile not normal file')
+if __name__ == "__main__":
+    print('Testing...')
+    conf = sb_config_gsettings()
+    conf.settings.set_boolean('test-setting', True)
+    conf.test()
+    print('done\n')
+    
